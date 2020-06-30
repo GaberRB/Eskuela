64a43bbf4899d12785bb29fb7598d5f64c726019
gabriel <gabriel@pop-os.localdomain>
2020-06-30 09:40:41 -0300

    Desafio 4 5

--- teachers.js
+++ teachers.js
@@ -66,7 +66,6 @@ exports.show = function(req, res){
 }
 
 //edit
-
 exports.edit = function(req, res){
     const { id } = req.params
 
@@ -83,5 +82,53 @@ exports.edit = function(req, res){
 
 
     return res.render('teachers/edit', {teacher})
+
+}
+
+//put
+exports.put = function(req, res){
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if (teacher.id == id){
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundTeacher) return res.send("Teacher not found!")
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+ 
+    data.teachers[index] = teacher
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send('Wrong File!')
+
+        return res.redirect(`/teachers/${id}`)
+    })
+}
+
+//delete
+exports.delete = function(req, res){
+    const { id } = req.body
+
+    const filteredTeacher = data.teachers.filter(function(teacher){
+        return teacher.id != id
+    })
+    
+    data.teachers = filteredTeacher
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send('Wrong File')
+
+        return res.redirect('/teachers')
+    })
+
 
 }
